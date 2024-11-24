79e6d344bfa53ed4b6f30bf4b6332134f7a3e267
FiollDoll <artemkadgi2@gmail.com>
2024-11-24 18:59:25 +0500

    build system add + some changes

--- Assets/Scripts/build/building.cs
+++ Assets/Scripts/build/building.cs
@@ -1,6 +1,28 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class building : MonoBehaviour
+public class Building : MonoBehaviour
 {
+    public Renderer MainRenderer;
+    public Vector2Int Size = Vector2Int.one;
 
+    public void SetTransparent(bool available)
+    {
+        MainRenderer.material.color = available ? Color.green : Color.red;
+    }
+
+    public void SetNormal() => MainRenderer.material.color = Color.white;
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 offset = new Vector3(-Size.x * 0.5f + 0.5f, 0, -Size.y * 0.5f + 0.5f);
+
+        for (int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y < Size.y; y++)
+            {
+                Gizmos.color = (x + y) % 2 == 0 ? new Color(0.88f, 0f, 1f, 0.3f) : new Color(1f, 0.68f, 0f, 0.3f);
+                Gizmos.DrawCube(transform.position + offset + new Vector3(x, 0, y), new Vector3(1, .1f, 1));
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/build/buildingSystem.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+    public Vector2Int GridSize = new Vector2Int(10, 10);
+    private Building[,] grid;
+    private Building flyingBuilding;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        grid = new Building[GridSize.x, GridSize.y];
+        mainCamera = Camera.main;
+    }
+
+    public void StartPlacingBuilding(Building buildingPrefab)
+    {
+        if (flyingBuilding != null)
+            Destroy(flyingBuilding.gameObject);
+
+        flyingBuilding = Instantiate(buildingPrefab);
+    }
+
+    private void Update()
+    {
+        if (flyingBuilding != null)
+        {
+            var groundPlane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (groundPlane.Raycast(ray, out float position))
+            {
+                Vector3 worldPosition = ray.GetPoint(position);
+
+                int x = Mathf.RoundToInt(worldPosition.x);
+                int y = Mathf.RoundToInt(worldPosition.z);
+
+                x -= flyingBuilding.Size.x / 2;
+                y -= flyingBuilding.Size.y / 2;
+
+                bool available = true;
+
+                if (x < -GridSize.x / 2 || x > GridSize.x / 2 - flyingBuilding.Size.x) available = false;
+                if (y < -GridSize.y / 2 || y > GridSize.y / 2 - flyingBuilding.Size.y) available = false;
+
+                if (available && IsPlaceTaken(x, y)) available = false;
+
+                flyingBuilding.transform.position = new Vector3(x + flyingBuilding.Size.x / 2f, 0, y + flyingBuilding.Size.y / 2f);
+                flyingBuilding.SetTransparent(available);
+
+                if (available && Input.GetMouseButtonDown(0))
+                    PlaceFlyingBuilding(x, y);
+            }
+        }
+    }
+
+    /*
+    private void OnDrawGizmosSelected()
+    {
+        // Визуализируем сетку
+        Gizmos.color = Color.green;
+        for (int x = -GridSize.x / 2; x < GridSize.x / 2; x++)
+        {
+            for (int y = -GridSize.y / 2; y < GridSize.y / 2; y++)
+            {
+                Vector3 position = new Vector3(x, 0, y);
+                Gizmos.DrawWireCube(position + new Vector3(0.5f, 0, 0.5f), new Vector3(1f, 0, 1f));
+            }
+        }
+    }
+    */
+
+    private bool IsPlaceTaken(int placeX, int placeY)
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+                if (grid[placeX + x, placeY + y] != null) return true;
+        }
+
+        return false;
+    }
+
+    private void PlaceFlyingBuilding(int placeX, int placeY)
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+                grid[placeX + x, placeY + y] = flyingBuilding;
+        }
+
+        flyingBuilding.SetNormal();
+        flyingBuilding = null;
+    }
+}
