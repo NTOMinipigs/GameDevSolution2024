ed4141b8209dd016d7ebc4320bf202ccd753d72d
FiollDoll <artemkadgi2@gmail.com>
2024-12-06 00:04:32 +0500

    add some scripts + some changes

---
+++ Assets/Scripts/preference.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preference : MonoBehaviour
+{
+    public float sensitivityOfCamera = 5f;
+    public float globalVolume = 100f;
+    public bool postProcessing = true;
+}
